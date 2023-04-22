ccba1185e6b896d6f6990e89e40f1a49e20595b1
VinoliaMoekwa <moe23001@byui.edu>
2023-04-22 23:18:23 +0200

    Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+
+        string userName = GetUserName();
+        int userNumber =GetUserNumber();
+
+        int squaredNumber = SquaredNumber(userNumber);
+
+        DispalyResult(userName,squaredNumber);
+        }
+            static void DisplayWelcomeMessage()
+        {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    static int GetUserNumber()
+    {
+        Console.Write("Please enter your favourite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number ;
+    }
+
+    static int SquaredNumber(int number)
+    {
+        int sqaure = number * number;
+        return sqaure;
+    }
+
+    static void DispalyResult(string name, int sqaure)
+    {
+        Console.WriteLine($"{name}, the square of your number is {sqaure}");
+
     }
 }
