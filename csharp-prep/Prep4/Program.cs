db381f80123144c0f170a5f483dfb7b293008d96
VinoliaMoekwa <moe23001@byui.edu>
2023-04-22 22:41:09 +0200

    number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+    
+        
+    int userChoice = -1;
+    while( userChoice != 0)
+    {
+        Console.Write("Enter number, enter 0 to quit: ");
+
+        string userInput = Console.ReadLine();
+        userChoice = int.Parse(userInput);
+    
+
+        if (userChoice != 0)
+        {
+            numbers.Add(userChoice);
+        }
     }
-}
+     
+     int sum = 0;
+     foreach(int number in numbers)
+     {
+        sum += number;
+     }
+
+     Console.WriteLine($"The sum is:{sum}");
+
+     float average = ((float)sum)/numbers.Count;
+     Console.WriteLine($"The average is:{average}");
+
+    int max = numbers[0];
+
+    foreach(int number in numbers)
+    {
+        if (number> max)
+        {
+            max = number;
+        }
+
+    }
+    Console.WriteLine($"The max is:{max}");
+        }
+
+    }
