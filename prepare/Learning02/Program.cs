6a01b32fd60e2fac5512ee2faaf964e339cd425c
VinoliaMoekwa <moe23001@byui.edu>
2023-04-27 23:19:45 +0200

    Completed Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
 
-class Program
+ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+    
+      Job job1 = new Job();
+      job1. _jobTitle= "Web Developer";
+      job1._company = "Kinetic Energy";
+      job1._startYear = 2018;
+      job1._endYear = 2020;
+
+      Job job2 = new Job();
+      job2._jobTitle = "Full Stack Developer";
+      job2._company = "Mukon";
+      job2._startYear = 2022;
+      job2._endYear =2023;
+
+      Resume myResume = new Resume();
+      myResume._name = "Ntombi Moekwa";
+
+      myResume._jobs.Add(job1);
+      myResume._jobs.Add(job2);
+
+      myResume.Display();
+
     }
 }
+
