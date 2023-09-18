bea61f13d7ee75cfd2dd5a5fb5b405c17b018ec8
VinoliaMoekwa <moe23001@byui.edu>
2023-09-18 16:32:34 +0200

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,45 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
+
+
+
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        //Console.WriteLine("Enter the magic number");
+        //int magicNumber =int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Enter your guess: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out guess))
+
+            {
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break; // Exit the loop when the guess is correct
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
     }
 }
