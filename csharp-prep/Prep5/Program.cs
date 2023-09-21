a721203433ef008fa81aebfe9adeda09deef478a
VinoliaMoekwa <moe23001@byui.edu>
2023-09-21 15:43:56 +0200

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string userName = PromptUserName();
+        int userNumber = PromtUserNumber();
+
+        int sqauredNumber = SqaureNumber(userNumber);
+
+        DisplayResults(  name, square);
     }
-}
+
+
+
+        static void DisplayWelcomeMessage(){
+            Console.WriteLine("Welcome to the program!");
+
+        }
+
+        static  string PromptUserName()
+        {
+            Console.Write("What is your name?");
+            string name =Console.ReadLine();
+
+            return name;
+            
+        }
+
+        static int PromtUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            int number =int.Parse(Console.ReadLine());
+
+            return number;
+        }
+ 
+        static int SqaureNumber (int number)
+        {
+            int sqaure = number * number;
+            return sqaure;
+        }
+
+        static void DisplayResults( string name, int sqaure)
+        {
+            Console.WriteLine($"{name} ,the sqaure of your number is {sqaure}");
+        }
+
+    }
